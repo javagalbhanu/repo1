5e77197a667c729fba47b2ba8e4786a604f38cc6
javagalbhanu <javagalbhanu@gmail.com>
2019-02-28 12:34:53 +0530

    Create XL.java

---
+++ XL.java
@@ -0,0 +1,18 @@
+package generic;
+
+import java.io.FileInputStream;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class XL {
+	public static String getData(String path,String sheet,int r,int c) {
+		String v="";
+		try {
+			Workbook wb = WorkbookFactory.create(new FileInputStream(path));
+			v=wb.getSheet(sheet).getRow(r).getCell(c).toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		return v;
+	}
+}
