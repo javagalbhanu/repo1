880957ad7ca12db0c01dacfdd6a4ccaa15a0d85d
javagalbhanu <javagalbhanu@gmail.com>
2019-03-01 12:08:18 +0530

    Update LoginPage.java

--- LoginPage.java
+++ LoginPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 
 public class LoginPage {
 	@FindBy(id="username")
@@ -34,7 +35,7 @@ public class LoginPage {
 		loginBTN.click();
 	}
 	
-	public void verifyErrMsgDisaplayed() {
-		
+	public void verifyErrMsgDisaplayed() {	
+		Assert.assertTrue(errMsg.isDisplayed());
 	}
 }
