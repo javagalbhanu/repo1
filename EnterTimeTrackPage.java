36d2137838c6f6a2d69f903542af859b709a4d60
javagalbhanu <javagalbhanu@gmail.com>
2019-03-01 12:35:42 +0530

    Update EnterTimeTrackPage.java

--- EnterTimeTrackPage.java
+++ EnterTimeTrackPage.java
@@ -4,6 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
 
 public class EnterTimeTrackPage {
 	private final String xp="(//div[contains(text(),'Settings')])[1]";
@@ -23,6 +27,18 @@ public class EnterTimeTrackPage {
 	
 	public void clickLicenses() {
 		licenses.click();
+	}
+	
+	public void verifyHomePageIsDisplayed(WebDriver driver,long time,String title) {
+		WebDriverWait wait=new WebDriverWait(driver,time);
+		try {
+				wait.until(ExpectedConditions.titleContains(title));
+				Reporter.log("Home Page is Displayed",true);
+		}
+		catch (Exception e) {
+			Reporter.log("HomePage is Not Displayed",true);
+			Assert.fail();
+		}
 	}
 }
 
